10156d88b0c68b7b7c2e5b76174a7cf3d9d0f4de
AlexiaAM <alexia77755@gmail.com>
2022-05-12 20:13:07 -0500

    camera active and save photos

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -16,8 +16,12 @@ import {
 import { camera, trash, close } from "ionicons/icons";
 import ExploreContainer from "../components/ExploreContainer";
 import "./Tab2.css";
+import { usePhotoGallery, UserPhoto } from '../hooks/usePhotoGallery';
+import { useState } from "react";
 
 const Tab2: React.FC = () => {
+    const { photos, takePhoto } = usePhotoGallery();
+    
     return (
         <IonPage>
             <IonHeader>
@@ -26,14 +30,17 @@ const Tab2: React.FC = () => {
                 </IonToolbar>
             </IonHeader>
             <IonContent fullscreen>
-                <IonHeader collapse="condense">
-                    <IonToolbar>
-                        <IonTitle size="large">Tab 2</IonTitle>
-                    </IonToolbar>
-                </IonHeader>
-                <ExploreContainer name="Tab 2 page" />
+                <IonGrid>
+                <IonRow>
+                {photos.map((photo, index) => (
+                    <IonCol size="6" key={index}>
+                    <IonImg src={photo.webviewPath} />
+                    </IonCol>
+                ))}
+                </IonRow>
+                </IonGrid>
                 <IonFab vertical="bottom" horizontal="center" slot="fixed">
-                    <IonFabButton onClick={() => {}}>
+                    <IonFabButton onClick={() => takePhoto()}>
                         <IonIcon icon={camera}></IonIcon>
                     </IonFabButton>
                 </IonFab>
